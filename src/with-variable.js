c2681ba2c5aa2e78e3f8b59d36bfcf919e41bac4
robsonbittencourt <robson.luizv@gmail.com>
2016-11-04 19:25:31 -0200

    Cria exemplo com variável

---
+++ src/with-variable.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const promise = require ('./promise-example');
+
+var asynResult = promise.doSomethingAsync();
+
+// do other random things
+var foo = 10;
+
+if (foo > 5) {
+  //nothing
+}
+
+asynResult.then(function(result) {
+  console.log(result);
+});
