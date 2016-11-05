cdaef166aa013c72b7fef601a0b1a79815ef0f7f
robsonbittencourt <robson.luizv@gmail.com>
2016-11-05 20:44:22 -0200

    Remove variável não utilizada

--- src/examples/9 - all.js
+++ src/examples/9 - all.js
@@ -3,7 +3,7 @@
 var q = require ('q');
 var promise = require ('../promise-creators/promise-q');
 
-var outputPromise = q.all([
+q.all([
   promise.doSomethingAsync(1),
   promise.doSomethingAsync(2),
   promise.doSomethingAsync(3)
